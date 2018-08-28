02d5f0c7e5b768a2fb6ac7cb2ab4cac1d7be8d4a
baho-utot <baho-utot@columbus.rr.com>
2018-08-27 20:54:29 -0400

    2018-08-27

--- chroot-lfs.sh
+++ chroot-lfs.sh
@@ -1,21 +1,39 @@
 #!/bin/bash -e
+##########################################
+#       Title: chroot-lfs.sh             #
+#        Date: 2018-08-27                #
+#     Version: 1.0                       #
+#      Author: baho-utot@columbus.rr.com #
+#     Options:                           #
+##########################################
+#
+#	This allows me to run LFS-8.2 under Fedora 28
+#
 MOUNTPOINT=/mnt
 MOUNTPARTITION=/dev/sdd4
 mount "${MOUNTPARTITION}" "${MOUNTPOINT}"
-
+#
+#	Mount filesystems
+#
 mount -v --bind /dev /mnt/dev
 mount -vt devpts devpts /mnt/dev/pts -o gid=5,mode=620
 mount -vt proc proc /mnt/proc
 mount -vt sysfs sysfs /mnt/sys
 mount -vt tmpfs tmpfs /mnt/run
-
+#
+#	Git it did
+#
 chroot /mnt /usr/bin/env -i HOME=/root TERM=$TERM PS1='(blfs builder) \u:\w\$ ' PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/bash --login
-
+#
+#	Cleanup
+#
 umount /mnt/run
 umount /mnt/sys
 umount /mnt/proc
 umount /mnt/dev/pts
 umount /mnt/dev
 umount /mnt
-
+#
+#	See it is clean
+#
 mount
