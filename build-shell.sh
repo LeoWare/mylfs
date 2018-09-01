b7e917475cbd3f7fdbf6e98d377135e035750bbb
Samuel Raynor <samuel@samuelraynor.com>
2018-08-31 17:13:06 -0700

    Changes.

--- build-shell.sh
+++ build-shell.sh
@@ -379,15 +379,15 @@ readline() {
     build " Change directory: BUILD" "pushd BUILD" ${_logfile}
     unpack "${PWD}" "${_pkgname}-${_pkgver}"
     build " Change directory: ${_pkgname}-${_pkgver}" "pushd ${_pkgname}-${_pkgver}" ${_logfile}
+    build "+ sed -i '/MV.*old/d' Makefile.in" "sed -i '/MV.*old/d' Makefile.in" ${_logfile}
+    build "+ sed -i '/{OLDSUFF}/c:' support/shlib-install" "sed -i '/{OLDSUFF}/c:' support/shlib-install" ${_logfile}
     build " Create work directory" "install -vdm 755 ../build" ${_logfile}
     build " Change directory: ../build" "pushd ../build" ${_logfile}
 
-    build "+ sed -i '/MV.*old/d' Makefile.in" "sed -i '/MV.*old/d' Makefile.in" ${_logfile}
-    build "+ sed -i '/{OLDSUFF}/c:' support/shlib-install" "sed -i '/{OLDSUFF}/c:' support/shlib-install" ${_logfile}
     build "+ ../${_pkgname}-${_pkgver}/configure --prefix=/usr --disable-static --docdir=/usr/share/doc/readline-7.0" "../${_pkgname}-${_pkgver}/configure --prefix=/usr --disable-static --docdir=/usr/share/doc/readline-7.0" ${_logfile}
-    build "+ make SHLIB_LIBS="-L/tools/lib -lncursesw"" "make SHLIB_LIBS="-L/tools/lib -lncursesw"" ${_logfile}
+    build "+ make SHLIB_LIBS=\"-L/tools/lib -lncursesw\"" "make SHLIB_LIBS=\"-L/tools/lib -lncursesw\"" ${_logfile}
 
-    build "+ make SHLIB_LIBS="-L/tools/lib -lncurses" install" "make SHLIB_LIBS="-L/tools/lib -lncurses" install" ${_logfile}
+    build "+ make SHLIB_LIBS=\"-L/tools/lib -lncurses\" install" "make SHLIB_LIBS=\"-L/tools/lib -lncurses\" install" ${_logfile}
     build "+ mv -v /usr/lib/lib{readline,history}.so.* /lib" "mv -v /usr/lib/lib{readline,history}.so.* /lib" ${_logfile}
     build "+ ln -sfv ../../lib/$(readlink /usr/lib/libreadline.so) /usr/lib/libreadline.so" "ln -sfv ../../lib/$(readlink /usr/lib/libreadline.so) /usr/lib/libreadline.so" ${_logfile}
     build "+ ln -sfv ../../lib/$(readlink /usr/lib/libhistory.so ) /usr/lib/libhistory.so" "ln -sfv ../../lib/$(readlink /usr/lib/libhistory.so ) /usr/lib/libhistory.so" ${_logfile}
@@ -458,7 +458,7 @@ EOF
     build "+ sed -i -e '/flex/s/as_fn_error/: ;; # &/' configure" "sed -i -e '/flex/s/as_fn_error/: ;; # &/' configure" ${_logfile}
     build "+ ../${_pkgname}-${_pkgver}/configure --prefix=/usr --with-readline --mandir=/usr/share/man --infodir=/usr/share/info" "../${_pkgname}-${_pkgver}/configure --prefix=/usr --with-readline --mandir=/usr/share/man --infodir=/usr/share/info" ${_logfile}
     build "+ make" "make" ${_logfile}
-    build "+ echo "quit" | ./bc/bc -l Test/checklib.b" "echo "quit" | ./bc/bc -l Test/checklib.b" ${_logfile}
+    build "+ echo \"quit\" | ./bc/bc -l Test/checklib.b" "echo \"quit\" | ./bc/bc -l Test/checklib.b" ${_logfile}
     build "+ make install" "make install" ${_logfile}
 
 
