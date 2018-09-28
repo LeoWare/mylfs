53594c84c16c9ca0cdf721f76a36a286e9f63f11
root <root@localhost.localdomain>
2018-09-28 14:29:52 -0700

    kernel config and chroot script

---
+++ go-chroot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ./function.inc
+
+msg_line "Mounting kernel virtual filesystems: "
+mount_kernel_vfs
+msg_success
+
+chroot "$LFS" /usr/bin/env -i          \
+    HOME=/root                         \
+    TERM="$TERM"                       \
+    PS1='(lfs chroot) \u:\w\$ '        \
+    PATH=/bin:/usr/bin:/sbin:/usr/sbin \
+    /bin/bash --login
+
+msg_line "Unmounting kernel virtual filesystems: "
+umount_kernel_vfs
+msg_success
