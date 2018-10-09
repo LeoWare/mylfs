a1dcd290ab4ad1f4d991ee180d26fc04fffbc5df
Samuel Raynor <samuel@samuelraynor.com>
2018-10-09 10:17:04 -0700

    fixed environment variable escapes

--- build-shell.sh
+++ build-shell.sh
@@ -2252,16 +2252,17 @@ nspr_build() {
     build " Change directory: BUILD" "pushd BUILD" ${_logfile}
     unpack "${PWD}" "${_pkgname}-${_pkgver}"
     build " Change directory: ${_pkgname}-${_pkgver}" "pushd ${_pkgname}-${_pkgver}" ${_logfile}
+    build "+ cd nspr" "cd nspr" ${_logfile}
     build "+ sed -ri 's#^(RELEASE_BINS =).*#\1#' pr/src/misc/Makefile.in" "sed -ri 's#^(RELEASE_BINS =).*#\1#' pr/src/misc/Makefile.in" ${_logfile}
-    build "+ sed -i 's#$(LIBRARY) ##'            config/rules.mk" "sed -i 's#$(LIBRARY) ##'            config/rules.mk" ${_logfile}
-    build " Create work directory" "install -vdm 755 ../build" ${_logfile}
-    build " Change directory: ../build" "pushd ../build" ${_logfile}
+    build "+ sed -i 's#\$(LIBRARY) ##'            config/rules.mk" "sed -i 's#\$(LIBRARY) ##'            config/rules.mk" ${_logfile}
+    #build " Create work directory" "install -vdm 755 ../build" ${_logfile}
+    #build " Change directory: ../build" "pushd ../build" ${_logfile}
     build "+ ./configure --prefix=/usr --with-mozilla --with-pthreads $([ $(uname -m) = x86_64 ] && echo --enable-64bit)" "./configure --prefix=/usr --with-mozilla --with-pthreads $([ $(uname -m) = x86_64 ] && echo --enable-64bit)" ${_logfile}
     build "+ make" "make" ${_logfile}
 
     build "+ make install" "make install" ${_logfile}
 
-    build " Restore directory" "popd " /dev/null
+    #build " Restore directory" "popd " /dev/null
     build " Restore directory" "popd " /dev/null
     build " Restore directory" "popd " /dev/null
     >  ${_complete}
@@ -2324,7 +2325,7 @@ popt_build() {
     return 0
 }
 
-readline() {
+readline_build() {
     local   _pkgname="readline"
     local   _pkgver="7.0"
     local   _complete="${PWD}/LOGS/${FUNCNAME}.completed"
